9cf976768f5c5de106deca355b74a48365f9f753
Erdem-Atik <erdematik8@gmail.com>
2022-09-28 13:57:55 +0300

    added hooks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form(props) {
+  const [name, setName] = useState(""); //use hook is initial name, name
   function handleSubmit(e) {
     //event handling
     console.log("test");
     e.preventDefault();
-    props.addTask("say hello");
+    props.addTask(name);
+    setName("");
   }
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
@@ -20,6 +26,8 @@ export default function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
